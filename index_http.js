e32c792292a43309b099c0cb580c5ec776dd2160
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-06-16 10:35:04 +0200

    more sh

--- index_http.js
+++ index_http.js
@@ -4,6 +4,6 @@ http.createServer((request, response) => {
 	response.writeHead(200, {'Content-Type': 'text/html'})
 	response.write('Hello World! Im here')
 	response.end()
-}).listen(4000, () => {
+}).listen(3000, () => {
 	console.log('Node Server kører på http://localhost:3000');
 }) 
